8bddfa4620c81705fe961fef074cb0c64ddfc384
mkelly1 <mkelly@his.com>
2019-10-22 11:45:24 -0400

    Margo added Order and Product classes

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private double total_cost;
+
+    @ManyToOne
+    @JoinTable(joinColumns = @JoinColumn (name="order_id"),
+            inverseJoinColumns = @JoinColumn(name="user_id"))
+    private User user;
+
+    @OneToMany
+    private Collection<Product> products;
+
+    public Order(){}
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getTotal_cost() {
+        return total_cost;
+    }
+
+    public void setTotal_cost(double total_cost) {
+        this.total_cost = total_cost;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Collection<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Collection<Product> products) {
+        this.products = products;
+    }
+
+    public double calculateCost(){
+        double total = 0;
+        ArrayList<Product> products = new ArrayList<>(getProducts());
+        for (Product prod : products){
+            total += prod.getPrice();
+        }
+        return total;
+    }
+
+    public int productQuantity(){
+        return getProducts().size();
+    }
+
+    public double shipping(){
+        double cost = calculateCost();
+        if(cost < 50){
+            double shippingCost =  (cost * .05);
+            cost += shippingCost;
+        }
+        return cost;
+    }
+}
---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String details;
+    private double  price;
+    private boolean active;
+
+    private static int quantity;
+
+    public Product(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public static int getQuantity() {
+        return quantity;
+    }
+
+    public static void setQuantity(int quantity) {
+        Product.quantity = quantity;
+    }
+}
