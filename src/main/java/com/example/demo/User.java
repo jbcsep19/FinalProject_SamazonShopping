fd0b57136af4f596b782416719be2de5a06df543
Surendra K Bijukchhe <skbijukchhe@gmail.com>
2019-10-23 16:04:30 -0400

    surendra

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -34,7 +34,7 @@ public class User {
     @Column(name="position")
     private String position;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(joinColumns = @JoinColumn(name="user_id"),
             inverseJoinColumns = @JoinColumn(name="role_id"))
     private Collection<Role> roles;
