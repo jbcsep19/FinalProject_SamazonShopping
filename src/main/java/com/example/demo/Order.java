c70bc9f7e59ad3535fd745b1ca21b5c4f923d11a
mkelly1 <mkelly@his.com>
2019-10-24 15:53:08 -0400

    MK- merged 3:50pm Thurs. changed list.html and productDetail.html to show image.

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -25,14 +25,14 @@ public class Order {
 
     public Order(){}
 
-    public double getTotal_cost() {
+ /*   public double getTotal_cost() {
         return total_cost;
     }
 
     public void setTotal_cost(double total_cost) {
         this.total_cost = total_cost;
     }
-
+*/
     public User getUser() {
         return user;
     }
